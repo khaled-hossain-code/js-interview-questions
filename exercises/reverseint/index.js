12f380e095c4d68ab5f6ea7cc5c5736e726befef
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-08-09 06:17:28 +0600

    reverse Int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  return Math.sign(n) * parseInt(n.toString().split('').reverse().join(''))
+}
 
+function reverseInt1(n) {
+  const sign = Math.sign(n);
+  return sign * Number(String(n * sign).split('').reverse().join(''));
+}
+
+reverseInt(-123)
 module.exports = reverseInt;
