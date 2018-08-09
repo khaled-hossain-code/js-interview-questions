a2b3b0a3148eea399af56f922742b6b4f30c2c38
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-08-09 07:14:47 +0600

    max char in str

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charDict = {};
 
+  for (let character of str) {
+    if(charDict[character]) {
+      charDict[character]++;
+    }else {
+      charDict[character] = 1;
+    }
+  }
+  
+  const keys = Object.keys(charDict);
+  const values = Object.values(charDict);
+  const index = values.indexOf(Math.max(...values))
+  return keys[index];
+
+}
+
+maxChar("aaaapple 1231111");
 module.exports = maxChar;
