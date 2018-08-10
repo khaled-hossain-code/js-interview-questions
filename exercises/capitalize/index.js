5aab2dc218b7c8c1fbd6397deb38aab48cdfeb97
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-08-10 20:23:28 +0600

    capitalize each word of sentence

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -4,9 +4,21 @@
 // return the capitalized string.
 // --- Examples
 //   capitalize('a short sentence') --> 'A Short Sentence'
-//   capitalize('a lazy fox') --> 'A Lazy Fox'
+  capitalize('a lazy fox')
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  return str.replace(/\b\w/g, l => l.toUpperCase());
+}
+
+function capitalize1(str) {
+  let strArr = str.split(' ');
+
+  strArr.forEach( (element, index, array) => {
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1)
+  });
+
+  return strArr.join(' ');
+}
 
 module.exports = capitalize;
