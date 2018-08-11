3d48715dadc989d8619169af41e93e754692aeda
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-08-11 10:40:42 +0600

    stairs

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -4,19 +4,36 @@
 // with N levels using the # character.  Make sure the
 // step has spaces on the right hand side!
 // --- Examples
-//   steps(2)
+  steps(2);
 //       '# '
 //       '##'
 //   steps(3)
 //       '#  '
 //       '## '
 //       '###'
-//   steps(4)
+  // steps(2)
 //       '#   '
 //       '##  '
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row=1) {
+  if(n < row){
+    return;
+  }
+
+  let step1 = '#'.repeat(row);
+  let empty = step1.padEnd(n);
+  console.log(`${empty}`);
+  steps(n, row + 1);
+}
+
+function steps1(n) {
+  for (let i=1; i<=n; i++){
+   let step1 = '#'.repeat(i);
+   let empty = step1.padEnd(n);
+   console.log(`${empty}`);
+  }
+}
 
 module.exports = steps;
